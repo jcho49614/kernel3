4e54cce67c367d5e328d6638c087c5fcd086c132
jcho49614 <jcho49614@gmail.com>
2026-04-04 02:10:22 +0900

    just one last edit

--- buildfiles/build.ps1
+++ buildfiles/build.ps1
@@ -1,4 +1,9 @@
-nasm "../bootloader.asm" -f bin -o "../binaryfiles/bin1.bin"
-nasm "../kernel.asm" -f bin -o "../binaryfiles/bin2.bin"
-cmd /c copy /b bin1.bin + bin2.bin "../os.img"
+$rootDir = (Get-Item "..").FullName
+
+nasm -i "$rootDir/" "../bootloader.asm" -f bin -o "../binaryfiles/bin1.bin"
+
+nasm -i "$rootDir/" "../kernel.asm" -f bin -o "../binaryfiles/bin2.bin"
+
+cmd /c "copy /b ..\binaryfiles\bin1.bin + ..\binaryfiles\bin2.bin ..\os.img"
+
 qemu-system-i386 -hda "../os.img"
