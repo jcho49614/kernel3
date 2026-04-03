d10242b90050cfbcf894eb776cbe3be032bfee9d
jcho49614 <jcho49614@gmail.com>
2026-04-03 16:49:20 +0900

    It works now yay it was a problem with the standardlibrary code itself apparantly it was just outputting and umping it into the cpu now its fixed

--- assembly_notes/standardlibrary.asm
+++ assembly_notes/standardlibrary.asm
@@ -8,16 +8,17 @@
 ;required registers: si, ax (will be pushed so not really)
 ;prints any string given
 
+
 print_string:
 	pusha					;push everything for safety
 	mov ah, 0x0e			;teletype
-	loop:
+	print_loop:
 		mov al, [si]		;first byte
 		cmp al, 0			;check if null
 		je stop_printing	;stop printing if null
 		int 0x10			;interrupt 0x10
 		inc si
-		jmp loop
+		jmp print_loop
 	stop_printing: 
 		popa
 		ret		;just return if stop printing
--- assembly_notes/testingstandard.asm
+++ assembly_notes/testingstandard.asm
@@ -10,32 +10,40 @@ mov ax, cs
 mov ds, ax
 mov es, ax
 
+jmp main
+
+;-----------------------------------------
+;just some libraries
 %include "standardlibrary.asm"
+msg1: db 'hello, world!' ,0
+msg2: db 'testing the print function' ,0
+;-----------------------------------------
 
-mov si, msg1
-call print_string
-call newline
+main:
+	mov si, msg1
+	call print_string
+	call newline
 
-mov si, msg2
-call print_string
-call newline
+	mov si, msg2
+	call print_string
+	call newline
 
-mov bx, 0x1234
-call print_hex
-call newspace
+	mov bx, 0x1234
+	call print_hex
+	call newspace
 
-mov bx, 0x5678
-call print_hex
-call newspace
+	mov bx, 0x5678
+	call print_hex
+	call newspace
 
-mov bx, 0x9abc
-call print_hex
-call newspace
+	mov bx, 0x9abc
+	call print_hex
+	call newspace
+	
+	jmp $
 
 
 ;---------------------------------
-msg1: db 'hello, world!' ,0
-msg2: db 'testing the print function' ,0
 
 times 510-($-$$) db 0
 dw 0xaa55
